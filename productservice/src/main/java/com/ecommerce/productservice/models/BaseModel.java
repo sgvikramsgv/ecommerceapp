424b7ed07ba11ca5d11ab997010381c113621da2
Vikram SG <sg.vikram@gmail.com>
2023-11-29 22:57:12 +0530

    Testing DB integration

--- productservice/src/main/java/com/ecommerce/productservice/models/BaseModel.java
+++ productservice/src/main/java/com/ecommerce/productservice/models/BaseModel.java
@@ -1,11 +1,18 @@
 package com.ecommerce.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseModel {
+    @Id
     private long id;
 
 }
