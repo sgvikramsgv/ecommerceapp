892aab1296c9ad60d96bc39133e405f2e9613744
Vikram SG <sg.vikram@gmail.com>
2023-12-01 21:12:00 +0530

    synching data

--- productservice/src/main/java/com/ecommerce/productservice/models/BaseModel.java
+++ productservice/src/main/java/com/ecommerce/productservice/models/BaseModel.java
@@ -1,11 +1,12 @@
 package com.ecommerce.productservice.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.GenericGenerators;
 import org.springframework.data.annotation.Id;
 
 @Getter
@@ -13,6 +14,9 @@ import org.springframework.data.annotation.Id;
 @MappedSuperclass
 public class BaseModel {
     @Id
-    private long id;
+    @GeneratedValue(generator = "uuidgenerator")
+    @GenericGenerator(name = "uuidgenerator")
+    @Column(name = "id", columnDefinition = "binary(16)", nullable = false, updatable = false)
+    private Long id;
 
 }
